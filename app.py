35085e1d1105a2f69661e4a591f841bf3f326576
Akshat <akshat.gupta@guavus.com>
2017-02-02 16:38:14 -0600

    decoupled reviews lookup

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
 def search():
 	return render_template('search.html')
 
-def make_api_call(search_query):
+def make_product_loopkup_api_call(search_query):
 	search_query = search_query.replace(' ', '%20')
 	url = "http://api.walmartlabs.com/v1/search?apiKey=vydf8ym75f468rbgwy5k5xwp&query=" + search_query
 	data = json.load(urllib2.urlopen(url))
@@ -30,26 +30,28 @@ def make_api_call(search_query):
 		item_id = item['itemId']
 		item_description = item['longDescription']
 		item_description = BeautifulSoup(item_description).text
-		review_url = "http://api.walmartlabs.com/v1/reviews/%s?apiKey=vydf8ym75f468rbgwy5k5xwp&format=json" % str(item_id)
-		review_data = json.load(urllib2.urlopen(review_url))
-		reviews = []
-		for review in review_data['reviews']:
-			reviews.append(review['reviewText'])
 
 		return_dict[item_id] = {
 			'item_name' : item_name,
 			'item_id' : item_id,
 			'item_url' : item_url,
-			'reviews' : reviews,
 			'item_description' : item_description,
 		}
 
 	return return_dict
 
+def make_product_reviews_api_call(item_id):
+	review_url = "http://api.walmartlabs.com/v1/reviews/%s?apiKey=vydf8ym75f468rbgwy5k5xwp&format=json" % str(item_id)
+	reviews = []
+	review_data = json.load(urllib2.urlopen(review_url))
+	for review in review_data['reviews']:
+		reviews.append(review['reviewText'])
+	return {'reviews' : reviews}
+
 @app.route('/request_handler', methods=['GET', 'POST'])
 def request_handler():
 	search_query = request.form['search_query']
-	items_dict = make_api_call(search_query)
+	items_dict = make_product_loopkup_api_call(search_query)
 	search_query_dict={'search_query' : search_query}
 	return render_template('search.html', items_dict=items_dict, search_query_dict=search_query_dict)
 
@@ -57,7 +59,9 @@ def request_handler():
 @app.route('/review_request_handler/<string:review_item_id>', methods=['POST'])
 def review_request_handler(review_item_id):
 	review_query = request.form['review_query']
-	print (review_query, review_item_id)
+	reviews_dict = make_product_reviews_api_call(review_item_id)
+	reviews_dict['review_query'] = review_query
+	print (reviews_dict)
 	return render_template('search.html', items_dict={}, search_query_dict={})
 
 if __name__ == '__main__':
