d0d00d7af4947e227c75009d47914c9aa9dd9d1d
Akshat <akshatgupta95@yahoo.com>
2017-03-05 13:44:41 -0600

    added proximity query search

--- indexer.py
+++ indexer.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import itertools
 import string
 import pickle
 import re
@@ -100,6 +101,53 @@ class Indexer:
 	                
 		return retrieved_docs
 
+	def _get_docs_phrase_query_word_range(self, search_query, item_id, word_range=1):
+		file_name = 'inv_idx%s.pickle' % item_id
+		with open(file_name, 'rb') as f:
+			inv_idx = pickle.load(f)
+		query_terms = [
+			re.sub(r'[^\w\s]','',q).lower()
+			for q in search_query.split(' ')
+		]
+	    
+		docs_to_pos = defaultdict(list)
+		for q_term in query_terms:
+			docs_to_pos_q_term = inv_idx[q_term]
+			if len(docs_to_pos_q_term) == 0:
+				return []
+			for doc_id in docs_to_pos_q_term.keys():
+				if doc_id in docs_to_pos.keys():
+					docs_to_pos[doc_id].append(docs_to_pos_q_term[doc_id])
+				else:
+					docs_to_pos[doc_id] = [docs_to_pos_q_term[doc_id]]
+
+		for k in docs_to_pos.keys():
+			if len(docs_to_pos[k]) < len(query_terms):
+				docs_to_pos.pop(k, None)
+	    
+		retrieved_docs = []
+
+		for doc_id in docs_to_pos.keys():
+			pos_list = docs_to_pos[doc_id]
+			idx_pos_tuples = []
+			for idx, curr_pos_list in enumerate(pos_list):
+				for p in curr_pos_list:
+					idx_pos_tuples.append((p, idx))
+			idx_pos_tuples = sorted(idx_pos_tuples, key=lambda x: x[0])
+			window_size = len(search_query.split(' '))
+			pos_combs = itertools.combinations(idx_pos_tuples, window_size)
+			for comb in pos_combs:
+				y_values = set([c[1] for c in comb])
+				if len(y_values) != window_size:
+					continue
+				else:
+					x_values = [c[0] for c in comb]
+					if max(x_values) - min(x_values) <= word_range:
+						retrieved_docs.append(doc_id)
+						break
+	                
+		return retrieved_docs
+
 	def index_documents(self):
 		inv_idx = self.create_inverted_index()
 		file_name = 'inv_idx%s.pickle' % self.item_id
@@ -110,4 +158,4 @@ class Indexer:
 		if search_type=='simple_query' or len(search_query.split(' ')) < 2:
 			return self._get_docs_simple_query(search_query, item_id)
 		else:
-			return self._get_docs_phrase_query(search_query, item_id)
+			return self._get_docs_phrase_query_word_range(search_query, item_id, word_range=5)
